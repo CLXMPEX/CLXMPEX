7b9135d70433c2b78d775b01c49cba91728d1a7c
CLXMPEX <jackyoukk123@gmail.com>
2026-05-14 20:40:01 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,7 +3,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
     Name = "COCK | AoT Revolution",
-    LoadingTitle = "CLXMPEX",
+    LoadingTitle = "DICKHEAD",
     LoadingSubtitle = "Attack on Titan Revolution",
     Theme = "Bloom",
     CustomTheme = {
